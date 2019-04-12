55ed1b96bf8bdf9d3c210a8a6e2a13f8fec79b71
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-11 19:38:13 -0600

    fix dims

--- gen_adv_serial.py
+++ gen_adv_serial.py
@@ -17,8 +17,8 @@ parser.add_argument('-m','--modelfile', required=True)
 args = parser.parse_args()
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train[:,None]
-x_test = x_test[:,None]
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 
 model_keras = load_model(args.modelfile)
 
