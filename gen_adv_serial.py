c64c48794ac6857d22606d8889ccb4d689d19894
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-11 19:59:51 -0600

    add adv gen graph

--- gen_adv_serial.py
+++ gen_adv_serial.py
@@ -11,6 +11,8 @@ from keras.datasets import mnist
 from cleverhans.attacks import FastGradientMethod
 from cleverhans.utils_keras import KerasModelWrapper
 
+tf.reset_default_graph()
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-m','--modelfile', required=True)
 
@@ -24,6 +26,9 @@ num_classes = 10
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
+sess = tf.Session()
+keras.backend.set_session(sess)
+
 model_keras = load_model(args.modelfile)
 
 score = model_keras.evaluate(x_test, y_test, verbose=0)
@@ -32,3 +37,6 @@ print('model test accuracy:', score[1])
 
 model_clever = KerasModelWrapper(model_keras)
 fgsm = FastGradientMethod(model_clever)
+
+gen_graph = fgsm.generate(x_train[0], eps=0.3, clip_min=0., clip_max=1.)
+print(gen_graph)
