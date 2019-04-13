0af7a6093ba5360cd177e0ee3c9186d02e5dcb9f
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-12 21:10:04 -0600

    change to pure tensorflow graph

--- make_mnist_cnn_tf.py
+++ make_mnist_cnn_tf.py
@@ -4,10 +4,10 @@ import numpy as np
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
-def cnn_model_fn(features, labels, mode):
+def cnn_model_fn(input_placeholder, labels, training=True):
     """Model function for CNN."""
     # Input Layer
-    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+    input_layer = tf.reshape(input_placeholder, [-1, 28, 28, 1])
 
     # Convolutional Layer #1
     conv1 = tf.layers.conv2d(
@@ -34,45 +34,31 @@ def cnn_model_fn(features, labels, mode):
     dense = tf.layers.dense(
         inputs=pool2_flat, units=1024, activation=tf.nn.relu)
     dropout = tf.layers.dropout(
-        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+        inputs=dense, rate=0.4, training=training)
 
     # Logits Layer
     logits = tf.layers.dense(inputs=dropout, units=10)
 
-    predictions = {
-        # Generate predictions (for PREDICT and EVAL mode)
-        "classes": tf.argmax(input=logits, axis=1),
-        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-        # `logging_hook`.
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    }
-
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+    classes = tf.argmax(input=logits, axis=1)
+    probabilities = tf.nn.softmax(logits, name="softmax_tensor")
 
     # Calculate Loss (for both TRAIN and EVAL modes)
     loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
 
     # Configure the Training Op (for TRAIN mode)
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-        train_op = optimizer.minimize(
-            loss=loss,
-            global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=tf.train.get_global_step(),
+        name='train_operation')
 
-    # Add evaluation metrics (for EVAL mode)
-    eval_metric_ops = {
-        "accuracy": tf.metrics.accuracy(
-            labels=labels, predictions=predictions["classes"])
-    }
-    return tf.estimator.EstimatorSpec(
-        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    return logits, classes, probabilities, loss, train_op
+
 
 if __name__ == '__main__':
-
-    mnist_classifier = tf.estimator.Estimator(
-        model_fn=cnn_model_fn, model_dir="/home/jsmith/AbsRefNNFramework/models/mnist_cnn_tf")
+    x = tf.placeholder(tf.float32, shape=(None, 28, 28))
+    y = tf.placeholder(tf.int32, shape=(None, 1))
+    logits, classes, probabilities, loss, train_op = cnn_model_fn(x, y)
 
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
     x_train = x_train/np.float32(255)
@@ -82,22 +68,22 @@ if __name__ == '__main__':
 
     print(x_train.shape, x_train.dtype)
 
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": x_train},
-        y=y_train,
-        batch_size=100,
-        num_epochs=None,
-        shuffle=True)
-
-    mnist_classifier.train(
-        input_fn=train_input_fn,
-        steps=10)
-
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": x_test},
-        y=y_test,
-        num_epochs=1,
-        shuffle=False)
-
-    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
-    print(eval_results)
+    # train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    #     x={"x": x_train},
+    #     y=y_train,
+    #     batch_size=100,
+    #     num_epochs=None,
+    #     shuffle=True)
+    #
+    # mnist_classifier.train(
+    #     input_fn=train_input_fn,
+    #     steps=10)
+    #
+    # eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    #     x={"x": x_test},
+    #     y=y_test,
+    #     num_epochs=1,
+    #     shuffle=False)
+    #
+    # eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+    # print(eval_results)
