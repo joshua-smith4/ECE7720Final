34a5470eeea705442bed60d28961dde6ebdb12cc
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-15 19:17:58 -0600

    try cublas in kernel

--- parallel_fgsm.cu
+++ parallel_fgsm.cu
@@ -1,12 +1,19 @@
 #include "cublas_v2.h"
 
-__global__ void doublify(float *a)
-{
-  int idx = threadIdx.x + threadIdx.y*4;
-  a[idx] *= 2;
-}
+__global__ void gpu_blas_mmul(const float *A, const float *B, float *C, const int m, const int k, const int n) {
+     int lda=m,ldb=k,ldc=m;
+     const float alf = 1;
+     const float bet = 0;
+     const float *alpha = &alf;
+     const float *beta = &bet;
 
-int main()
-{
-  return 0;
+     // Create a handle for CUBLAS
+     cublasHandle_t handle;
+     cublasCreate(&handle);
+
+     // Do the actual multiplication
+     cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
+
+     // Destroy the handle
+     cublasDestroy(handle);
 }
